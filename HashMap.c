8e326b4f44b38550ddf75e22c9eab9ab72d4a64d
wiirlock <wiirlock@gmail.com>
2017-04-22 14:08:06 +0300

    fix and add delete function

--- HashMap.c
+++ HashMap.c
@@ -9,6 +9,7 @@ typedef struct node
 	char *key;
 	char *val;
 	struct node *next;
+	struct node *parent;
 } node;
 
 static node *hashlist[HASHSIZE];
@@ -37,33 +38,63 @@ node
 }
 
 node 
+*add_to_node(node *nd, char *key, char *val) {
+	node *tmp = nd->next;
+	if (tmp == NULL)
+	{
+		tmp = (node*)malloc(sizeof(node));
+		tmp->key = key;
+		tmp->val = val;
+		tmp->next = NULL;
+		tmp->parent = nd;
+	}  else {
+		tmp->next = add_to_node(tmp, key, val);
+	}
+
+	return tmp;
+}
+
+node 
 *insert(char *key, char *val) {
 	node *nd;
 	unsigned hash_value;
-	if ((nd = find_by_key(key)) == NULL)
+
+	if ((nd = hashlist[get_hash(key)]) == NULL)
 	{
 		nd = (node*)malloc(sizeof(node));
-		if (nd == NULL || (nd->key = strdup(key)) == NULL)
-		{
+		if (nd == NULL || (nd->key = strdup(key)) == NULL) {
 			return NULL;
 		} else {
 			hash_value = get_hash(key);
-			nd->next = hashlist[hash_value];
+			nd->next = NULL;
+			nd->parent = hashlist[hash_value];
 			hashlist[hash_value] = nd;
 			nd->val = strdup(val);
 		}
 	} else {
-		free((void *) nd->val);
-		if ((nd->val = strdup(val)) == NULL)
-		{
-			return NULL;
+		if (strcmp(key, nd->key) == 0) {
+			free((void *) nd->val);
+			nd->val = strdup(val);
+		} else {
+			nd->next = add_to_node(nd, key, val);
 		}
+		
 	}
 	return nd;
 }
 
+void delete_by_key(char *key) {
+	node *nd = find_by_key(key);
+	nd->parent->next = nd->next;
+	free(nd);
+}
+
 char *get_value_by(char *key) {
 	node *nd = find_by_key(key);
+	if (nd == NULL)
+	{
+		return "Not found";
+	}
 	return nd->val;
 }
 
@@ -72,8 +103,15 @@ int main(int argc, char const *argv[])
 	insert("name", "Denis");
 	insert("age", "29");
 	insert("job", "Developer");
+	insert("id", "001");
+	insert("project", "Skynet");
+	insert("sex", "male");
+	insert("kids", "yes");
 
-	char *val = get_value_by("job");
+	delete_by_key("age");
+
+	char *val = get_value_by("age");
+
 	if (val != NULL)
 	{
 		printf("%s\n", val);
